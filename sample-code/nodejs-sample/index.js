82d87d6fd54853f0b175ce87d38ac630b96c1a3d
gitName <a@b.c>
2021-10-07 22:27:35 +0530

    docker

--- sample-code/nodejs-sample/index.js
+++ sample-code/nodejs-sample/index.js
@@ -1,4 +1,16 @@
+'use strict';
 
-const x=100;
-console.log(x);
-//test
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
